9e6d92efb01d76a4c6326d6ca624de3cdbe2232a
Dona <dona.denkey_dandjein@edu.devinci.fr>
2021-10-14 20:31:20 +0200

    finish dark mode

---
+++ js/black.js
@@ -0,0 +1,16 @@
+// MENU BURGER
+function myAnimation(x) {
+    x.classList.toggle("animate");
+}
+// prompt('Avez-vous obtenu un code sur le jeu ? Si oui, lequel ?');
+
+function myBurgerFunction() {
+    var menu = document.getElementById("burgermenu");
+    menu.classList.toggle("showmenu");
+}
+
+// POPUP
+
+function myPopUp() {
+    document.getElementById('myPopup').style.display = "block";
+}
